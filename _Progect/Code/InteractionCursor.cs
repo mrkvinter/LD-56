fdee83e33adfd7c3f57691e746ff213aedb0ca9f
Arseniy Rogovoy <mr.kvinter@gmail.com>
2024-10-27 09:21:12 +0200

    Transparent cursor for web

--- _Progect/Code/InteractionCursor.cs
+++ _Progect/Code/InteractionCursor.cs
@@ -10,11 +10,13 @@ namespace Game.Scripts
         
         private PointerType pointerType;
         private RectTransform rectTransform;
+        private Texture2D transparentTexture;
         
         private void Awake()
         {
             Cursor.visible = false;
             rectTransform = GetComponent<RectTransform>();
+            transparentTexture = CreateTransparentTexture(1, 1);
         }
 
         protected void LateUpdate()
@@ -23,6 +25,7 @@ namespace Game.Scripts
             if (rayCamera == null) return;
 
             Cursor.visible = false;
+            Cursor.SetCursor(transparentTexture, Vector2.zero, CursorMode.Auto);
             var screenPosition = Input.mousePosition;
             rectTransform.position = screenPosition;
         }
@@ -38,6 +41,22 @@ namespace Game.Scripts
             @default.SetActive(pointerType == PointerType.Default);
             up.SetActive(pointerType == PointerType.Up);
             down.SetActive(pointerType == PointerType.Down);
+        }
+        
+        private Texture2D CreateTransparentTexture(int width, int height)
+        {
+            var texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            var pixels = new Color[width * height];
+
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = new Color(0, 0, 0, 0); 
+            }
+
+            texture.SetPixels(pixels);
+            texture.Apply();
+
+            return texture;
         }
 
         public enum PointerType
